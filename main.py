6409d3f3e8b4f524feb74e29d473e284b6300b00
SSR100700 <72198465+SSR100700@users.noreply.github.com>
2020-10-02 07:27:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import time
 
 poke_list = ['Zygarde', 'Cosmog', 'Cosmoem', 'Solgaleo', 'Lunala', 'Necrozma', 'Magearna', 'Marshadow'
 , 'Greninja', 'Sylveon', 'Xerneas', 'Yveltal', 'Talonflame', 'Moltres', 'Charizard', 'Dragonite'
-, 'Alakazam', 'Ninjask', 'Shedinja', 'Snorlax', 'Mawile', 'Aerodactyl', 'Hoopa',  'Tyranitar', 'Hydreigon', 'Kangaskhan'
+, 'Alakazam', 'Ninjask', 'Shedinja', 'Snorlax', 'Mawile', 'Aerodactyl', 'Hoopa', 'Mewtwo', 'Kyurem', 'Shiny'
 ]
 
 #Thanks Anil Vro
